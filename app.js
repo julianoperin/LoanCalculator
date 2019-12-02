4df802ca735754c72883d95482b59eeaf2b07aa9
Juliano Perin <julianoperins@gmail.com>
2019-12-01 22:18:45 -0500

    Selected all elements

---
+++ app.js
@@ -0,0 +1,27 @@
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', function(e){
+    // Hide results
+    document.getElementById('results').style.display = 'none';
+    
+    // Show loader
+    document.getElementById('loading').style.display = 'block';
+  
+    setTimeout(calculateResults, 2000);
+  
+    e.preventDefault();
+  });
+  
+  // Calculate Results
+  function calculateResults(){
+    console.log('Calculating...');
+    // UI Vars
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+  
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
